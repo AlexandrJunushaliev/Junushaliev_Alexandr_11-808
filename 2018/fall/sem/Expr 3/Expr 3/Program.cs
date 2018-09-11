4ef3dc524f70f8cab39c578a5e866a8fdf0dcd5d
Alexandr Junushaliev <bazilhan.abdulaev@yandex.ru>
2018-09-11 15:53:07 +0300

    1

---
+++ 2018/fall/sem/Expr 3/Expr 3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expr_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = Console.ReadLine();
+            var b = a.Split(':');
+            var minutes = int.Parse(b[1]);
+            var hours = int.Parse(b[0]);
+            if (hours > 12) hours = hours - 12;
+            var angleOfHourArrow = hours * 30 + minutes * 0.5;
+            var angleOfMinuteArrow = minutes * 6;
+            var angleBetween = Math.Abs(angleOfHourArrow - angleOfMinuteArrow);
+            if (angleBetween > 180) angleBetween = 360 - angleBetween;
+            Console.WriteLine(angleBetween);
+        }
+    }
+}
