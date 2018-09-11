c285540e606a111d1e13f05e1aaa13e73b2dd2ee
Alexandr Junushaliev <bazilhan.abdulaev@yandex.ru>
2018-09-11 15:27:30 +0300

    1

---
+++ Expr_1/Expr 1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expr_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = int.Parse(Console.ReadLine());
+            var b = int.Parse(Console.ReadLine());
+            //var c = a;
+            //a = b;
+            //b = c;
+            if (a>b)
+            {
+                a = a - b;
+                b = b + a;
+                a = b - a;
+            }
+            else
+            {
+                b = b - a;
+                a = b + a;
+                b = a - b;
+            }
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+        }
+    }
+}
