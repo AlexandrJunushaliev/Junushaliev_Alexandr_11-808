a02e03332e1b92233023fc1bf9987dc8c4016512
Alexandr Junushaliev <bazilhan.abdulaev@yandex.ru>
2018-09-12 22:09:13 +0300

    1

---
+++ 2018/fall/pr/Biliard/BilliardsTask.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Billiards
+{
+    public static class BilliardsTask
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="directionRadians">Угол направелния движения шара</param>
+        /// <param name="wallInclinationRadians">Угол</param>
+        /// <returns></returns>
+        public static double TurnRadToDegrees(double ang)
+        {
+            return (ang * 180) / Math.PI;
+        }
+        public static double TurnDegreesToRad(double ang)
+        {
+            return (ang * Math.PI) / 180;
+        }
+        public static double BounceWall(double directionRadians, double wallInclinationRadians)
+        {
+            return TurnDegreesToRad(360.0 - TurnRadToDegrees(directionRadians) + 2.0 * TurnRadToDegrees(wallInclinationRadians));
+        }
+    }
+}
