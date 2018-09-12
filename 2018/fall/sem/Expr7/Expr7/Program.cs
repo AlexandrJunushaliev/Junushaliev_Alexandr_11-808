43945821621615a54d920666d9f782febe287c7a
Alexandr Junushaliev <bazilhan.abdulaev@yandex.ru>
2018-09-12 12:17:05 +0300

    1

--- 2018/fall/sem/Expr7/Expr7/Program.cs
+++ 2018/fall/sem/Expr7/Expr7/Program.cs
@@ -15,12 +15,14 @@ namespace Expr7
             var k = int.Parse(Console.ReadLine());
             Console.Write("b=");
             var b = int.Parse(Console.ReadLine());
-            var y1 = k * 1 + b+1;
-            var y2 = k * 2 + b+1;
-            var vector = new int[2];
-            vector[0] = 1;
-            vector[1] = y2 - y1;
-            Console.WriteLine("vector = ("+vector[0].ToString() + ';' + vector[1].ToString()+')');           
+            //var y1 = k * 1 + b+1;
+            //var y2 = k * 2 + b+1;
+            //var vector = new int[2];
+            //vector[0] = 1;
+            //vector[1] = y2 - y1;
+
+            Console.WriteLine("parallel vector = (1" + ';' + k+')');
+            Console.WriteLine("perpendicular vector  = (1" + ';' + -(1.0/k) + ')');
         }
     }
 }
