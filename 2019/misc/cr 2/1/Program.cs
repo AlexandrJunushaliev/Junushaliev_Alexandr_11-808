a302fefc2ed246cad5e6fc3a766937d8deb4333c
Hikirangi <dzhunall@mail.ru>
2019-05-14 13:39:22 +0300

    [ADD] cr2

---
+++ 2019/misc/cr 2/1/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Threading;
+
+namespace ConsoleApp42
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            
+        }
+        static async Task<List<string>> Task1(string path)
+        {
+            var fileInfos = new List<string>();
+            await Task.Run(() =>
+            {
+                
+                var info = new DirectoryInfo(path);
+                var dirs = info.GetDirectories();
+
+                foreach (var dir in dirs)
+                {
+                    var files = dir.GetFiles();
+                    foreach (var file in files)
+                    {
+                        fileInfos.Add(file.Name);
+                    }
+                }
+            });
+            return fileInfos;
+        }
+        static async void Task2(string path)
+        {
+            var answer = new StringBuilder();
+            var dis = new DirectoryInfo(path);
+            foreach (var file in dis.GetFiles())
+            {
+                var text = "";
+                using (StreamReader sr = file.OpenText())
+                {
+                    text = sr.ReadToEnd();
+                }
+                var parts = text.Split(';');
+                var className = parts[0];
+                var methodName = parts[1];
+                var argsNumber = (parts.Length - 2) / 2;
+                var args = new object[argsNumber];
+                for (var i=0;i<argsNumber;i++)
+                {
+                    var argStringType = parts[2 + i * 2];
+                    var argType = Type.GetType(argStringType, false, true);
+                    var argumentConstructor = argStringType
+                }
+            }
+
+        }
+    }
+}
---
+++ 2019/misc/cr 2/2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Newtonsoft;
+using Newtonsoft.Json;
+
+namespace ConsoleApp43
+{
+    class Comment
+    {
+        public int PostId { get; set; }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Body { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var url = string.Format("https://jsonplaceholder.typicode.com/comments");
+            WebRequest request = WebRequest.Create(url);
+            WebResponse response = request.GetResponse();
+            Task<string> answer = null;
+            using (Stream s = response.GetResponseStream())
+            {
+                using (StreamReader reader = new StreamReader(s))
+                {
+                    answer = reader.ReadToEndAsync();
+                }
+            }
+            var a = answer.Result;
+            var comments = JsonConvert.DeserializeObject<Comment[]>(a);
+            var needComments = comments.Where(v => v.Id % 2 == 0);
+            Count(needComments);
+            
+        }
+        static void Count(IEnumerable<Comment> needComments)
+        {
+            foreach (var comment in needComments)
+            {
+                Console.WriteLine(comment.Body.Length);
+            }
+        }
+    }
+}
