b62b402ccc9e2e70d2492d3359efb50f57ba58ac
Hikirangi <dzhunall@mail.ru>
2019-05-14 20:12:19 +0300

    [ADD] First Task

--- 2019/misc/cr 2/1/Program.cs
+++ 2019/misc/cr 2/1/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 using System.Threading;
+using System.Reflection;
 
 namespace ConsoleApp42
 {
@@ -13,14 +14,14 @@ namespace ConsoleApp42
 
         static void Main(string[] args)
         {
-            
+
         }
         static async Task<List<string>> Task1(string path)
         {
             var fileInfos = new List<string>();
             await Task.Run(() =>
             {
-                
+
                 var info = new DirectoryInfo(path);
                 var dirs = info.GetDirectories();
 
@@ -35,9 +36,10 @@ namespace ConsoleApp42
             });
             return fileInfos;
         }
-        static async void Task2(string path)
+        static void Task2(string path, string[] names)
         {
             var answer = new StringBuilder();
+            var res = new List<object>();
             var dis = new DirectoryInfo(path);
             foreach (var file in dis.GetFiles())
             {
@@ -47,18 +49,56 @@ namespace ConsoleApp42
                     text = sr.ReadToEnd();
                 }
                 var parts = text.Split(';');
+                //для название класса;название метода;тип аргумента1;строковое представление аргумента1;тип аргумента2;...
                 var className = parts[0];
                 var methodName = parts[1];
                 var argsNumber = (parts.Length - 2) / 2;
                 var args = new object[argsNumber];
-                for (var i=0;i<argsNumber;i++)
+                for (var i = 0; i < argsNumber; i++)
                 {
                     var argStringType = parts[2 + i * 2];
                     var argType = Type.GetType(argStringType, false, true);
-                    var argumentConstructor = argStringType
+                    var argConstructor = argType.GetConstructor(new Type[] { typeof(string) });
+                    string argConstructorArg = parts[2 + i * 2 + 1];
+                    var argObj = argConstructor.Invoke(new object[] { argConstructorArg });
+                    args[i] = argObj;
+                }
+                var classType = Type.GetType(className, false, true);
+                var constructor = classType.GetConstructor(new Type[] { });
+                dynamic obj = constructor.Invoke(new object[] { });
+                var method = classType.GetMethod(methodName);
+                dynamic result = classType.InvokeMember(method.Name, BindingFlags.InvokeMethod, null, obj, args);
+                res.Add(result);
+            }
+            Task3(path, names, res);
+        }
+        static void Task3(string path, string[] names, List<object> res)
+        {
+            foreach (var name in names)
+            {
+                var newPath = path + "\\name";
+                if (Directory.Exists(newPath))
+                {
+                    var i = 0;
+                    var right = 0;
+                    var dis = new DirectoryInfo(path);
+                    foreach (var file in dis.GetFiles())
+                    {
+                        string text;
+                        using (StreamReader sr = new StreamReader(file.DirectoryName))
+                        {
+                            text = sr.ReadToEnd();
+                        }
+
+                        if (text == res[i].ToString())
+                        {
+                            right++;
+                        }
+                        i++;
+                    }
+                    Console.WriteLine($"{name} - {right}");
                 }
             }
-
         }
     }
 }
